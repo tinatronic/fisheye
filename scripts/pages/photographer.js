3838b05fdc5fcfb4786750647f5d151cccccc87e
Justine Patoux <justine.patoux@gmail.com>
2024-04-20 16:27:14 +0200

    feat: added displayheader function

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,18 @@
-//Mettre le code JavaScript lié à la page photographer.html
+import * as headerTemplate from "/templates/headerTemplate.js";
+
+const url = new URLSearchParams(document.location.search);
+const photographerID = parseInt(url.get("id"));
+console.log(photographerID)
+
+async function displayHeader(photographers) {
+    const headerModel = headerTemplate(photographers);
+    const photographerDOM = headerModel.getUserHeaderDOM();
+    return photographerDOM;
+}
+
+async function init() {
+    const { photographers } = await getData();
+    displayHeader(photographers);
+}
+
+init();
