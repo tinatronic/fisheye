e79fc77a65d46c64bd71425408139139b3876c2a
Justine Patoux <justine.patoux@gmail.com>
2024-04-20 16:27:49 +0200

    fix: changed fetch function to add a catch

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,13 +1,24 @@
-    async function getPhotographers() {
-        const allData = (await fetch('../data/photographers.json')).json();
-        return allData;
-    }
+    // async function getPhotographers() {
+    //     // const allData = (await fetch('../data/photographers.json')).json();
+    //     // console.log(allData);
+    //     // return allData;
+    //     try {
+    //         const res = await fetch('../data/photographers.json');
+    //         const data = await res.json();
+    //         return data;
+    //         // const photographers = data.photographers;
+    //         // console.log(photographers)
+    //         // return photographers;
+    //     } catch (err) {
+    //         throw new Error("Error fetching data:", err);
+    //     }
+    // }
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
         photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
+            const photographerModel = homeTemplate(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
         });
@@ -15,9 +26,8 @@
 
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
+        const { photographers } = await getData();
         displayData(photographers);
     }
     
     init();
-    
